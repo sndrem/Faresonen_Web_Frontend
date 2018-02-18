6a2780c4aafdce007e4e55dce66e2ea76ab0355b
Sindre Moldeklev <sndrem@gmail.com>
2018-02-18 22:44:58 +0100

    List out goals scorers with name and time of goal

--- src/Components/Matches/FinishedMatchElements.js
+++ src/Components/Matches/FinishedMatchElements.js
@@ -10,7 +10,6 @@ class FinishedMatchElements extends Component {
 			goalScorersAwayTeam: []
 
 		}
-		// console.table(props.matchInfo);
 	}
 
 	componentDidMount() {
@@ -23,27 +22,105 @@ class FinishedMatchElements extends Component {
 
 	getGoals(eventUri) {
 		axios.get(eventUri).then(data => {
-			const goalEvents = this.filterGoalEvents(data.data.event);
+			const goalEvents = this.filterGoalEvents(data.data.event)
+			const calculatedEvents = this.calculateHomeAndAwayEvents(goalEvents);
+			this.extractPersonNames(calculatedEvents.home)
+				.then(data => {
+					this.setState({
+						goalScorersHomeTeam: data
+					})
+				});
+			this.extractPersonNames(calculatedEvents.away)
+				.then(data => {
+					this.setState({
+						goalScorersAwayTeam: data
+					})
+				});
 			
 		}).catch(err => {
 			console.error(err);
 		})
 	}
 
+	extractPersonNames(events) {
+		return new Promise((resolve, reject) => {
+			const promises = [];
+			events.forEach(e => {
+				promises.push(this.getNameOfPerson(e.person1['@uri']));
+			})
+
+			axios.all(promises).then(data => {
+				events.forEach((e, index, list) => {
+					e.person1 = data[index].data;
+				});
+				resolve(events);
+			});	
+		});
+	}
+
+
+
+	calculateHomeAndAwayEvents(events) {
+		return events.reduce((obj, e, index, list) => {
+			const prevElementA = list[index - 1] === undefined ? null : list[index - 1].goalsTeamA;
+			const prevElementB = list[index - 1] === undefined ? null : list[index - 1].goalsTeamB;
+			
+			const previousHomeScore = prevElementA === null ? 0 : parseInt(prevElementA, 10);
+			const previousAwayScore = prevElementB === null ? 0 : parseInt(prevElementB, 10);
+
+			const homeScore = parseInt(e.goalsTeamA, 10);
+			const awayScore = parseInt(e.goalsTeamB, 10);
+
+			if(homeScore > previousHomeScore) {
+				obj.home.push(e);
+			}
+
+			if(awayScore > previousAwayScore) {
+				obj.away.push(e);
+			}
+			
+
+			return obj;
+			// return obj;
+		}, { home: [], away: []});
+	}
+
 	filterGoalEvents(events) {
-		return events.filter(e => e.eventtype['@uri'].includes('http://api.tv2.no/sport/resources/eventtypes/3/'));
+		return events.filter(e => {
+			// http://api.tv2.no/sport/resources/eventtypes/3/ = mål
+			// http://api.tv2.no/sport/resources/extendedeventtypes/304/ = straffer
+			const eventType = e.eventtype['@uri'];
+			const extendedEventType = e.extendedeventtype['@uri'];
+			return eventType.includes('http://api.tv2.no/sport/resources/eventtypes/3/') || extendedEventType.includes('http://api.tv2.no/sport/resources/extendedeventtypes/304/');
+		});
+	}
+
+	getNameOfPerson(uri) {
+		return axios.get(uri);
+	}
+
+	formatGoalScoreText(scorers) {
+		return scorers.map((scorer) => {
+			if(scorer.extendedeventtype['@uri'].includes('http://api.tv2.no/sport/resources/extendedeventtypes/304/')) {
+				return `${scorer.person1.lastname} (str. ${scorer.eventtime})`;
+			} else {
+				return `${scorer.person1.lastname} (${scorer.eventtime})`; 
+			}
+		}).join(', ');
 	}
 
 	render() {
-		const teamNames = this.splitGameName(this.props.matchInfo.name);
+		const [home, away] = this.splitGameName(this.props.matchInfo.name);
+		const homeScorers = this.formatGoalScoreText(this.state.goalScorersHomeTeam);
+		const awayScorers = this.formatGoalScoreText(this.state.goalScorersAwayTeam);
 
 		return (
 			<div>
 				<Item>
 					<Item.Content>
-						<Item.Header>{teamNames[0]} {this.props.matchInfo.goalsTeamAEndtime} - {this.props.matchInfo.goalsTeamBEndtime} {teamNames[1]}</Item.Header>
-						<Item.Description>Målscorer A:</Item.Description>
-						<Item.Description>Målscorer B:</Item.Description>
+						<Item.Header>{ home } { this.props.matchInfo.goalsTeamAEndtime } - { this.props.matchInfo.goalsTeamBEndtime } { away }</Item.Header>
+						<Item.Description>{ home }: { homeScorers }</Item.Description>
+						<Item.Description>{ away }: { awayScorers }</Item.Description>
 					</Item.Content>
 				</Item>
 				<Divider /> 
