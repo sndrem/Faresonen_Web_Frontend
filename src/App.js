9a8da95b6861ae1017266962755108d7faa08c00
Sindre Moldeklev <sndrem@gmail.com>
2018-02-21 09:54:57 +0100

    Linted everything

--- src/App.js
+++ src/App.js
@@ -1,30 +1,55 @@
-import React, { Component } from 'react';
-import { Container } from 'semantic-ui-react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import FrontpageView from './Views/FrontpageView';
-import MainView from './Views/MainView';
-import RoundView from './Views/RoundView';
+import React, { Component } from "react";
+import { Container } from "semantic-ui-react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import FrontpageView from "./Views/FrontpageView";
+import MainView from "./Views/MainView";
+import RoundView from "./Views/RoundView";
 
 class App extends Component {
-
   state = {
-      leagueName: '',
-      tournamentId: '',
-      seasonId: ''
-  }
+    leagueName: "",
+    tournamentId: "",
+    seasonId: ""
+  };
 
   switchLeagueName = (leagueName, tournamentId, seasonId) => {
-    this.setState({ leagueName, tournamentId, seasonId});
-  }  
+    this.setState({ leagueName, tournamentId, seasonId });
+  };
 
   render() {
     return (
       <Container>
         <BrowserRouter>
           <Switch>
-            <Route exact path='/' component={(props) => <FrontpageView switchLeagueName={this.switchLeagueName.bind(this)} leagueInfo={this.state} {...props}/>}/>
-            <Route exact path='/league/:leagueName/:tournamentId/:seasonId' component={(props) => <MainView {...props}/>}/>
-            <Route exact path='/league/:tournamentId/:seasonId/:leagueName/round/:roundNumber/roundId/:roundId/nextRound/:nextRoundId' render={(props) => <RoundView tournamentId={this.state.tournamentId} seasonId={this.state.seasonId} leagueName={this.state.leagueName} switchLeagueName={this.switchLeagueName.bind(this)} {...props}/>}/>
+            <Route
+              exact
+              path="/"
+              component={props => (
+                <FrontpageView
+                  switchLeagueName={this.switchLeagueName}
+                  leagueInfo={this.state}
+                  {...props}
+                />
+              )}
+            />
+            <Route
+              exact
+              path="/league/:leagueName/:tournamentId/:seasonId"
+              component={props => <MainView {...props} />}
+            />
+            <Route
+              exact
+              path="/league/:tournamentId/:seasonId/:leagueName/round/:roundNumber/roundId/:roundId/nextRound/:nextRoundId"
+              render={props => (
+                <RoundView
+                  tournamentId={this.state.tournamentId}
+                  seasonId={this.state.seasonId}
+                  leagueName={this.state.leagueName}
+                  switchLeagueName={this.switchLeagueName}
+                  {...props}
+                />
+              )}
+            />
           </Switch>
         </BrowserRouter>
       </Container>
