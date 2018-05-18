7b404888a36d659904c7541882cab530556438f9
Sindre Moldeklev <sndrem@gmail.com>
2018-05-18 11:55:44 +0200

    Removed a console.log

--- src/databaseConfig/firebaseConfig.js
+++ src/databaseConfig/firebaseConfig.js
@@ -1,6 +1,5 @@
 import firebase from "firebase";
 
-console.log(process.env);
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -9,6 +8,5 @@ const config = {
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
 };
-console.log("Initializing Firebase");
 const fire = firebase.initializeApp(config);
 export default fire;
