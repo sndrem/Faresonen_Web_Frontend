4dff13a5755f7f75a5e3ea836574523389e4613d
Sindre Moldeklev <sindre.moldeklev@bekk.no>
2018-09-13 21:14:40 +0200

    Added logging with Raven and Sentry

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Raven from "raven-js";
 
 import "./index.css";
 import "./print.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+
+Raven.config(
+  "https://6804db3180e14d51a1da9c443b1a288c@sentry.io/1280627"
+).install();
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
