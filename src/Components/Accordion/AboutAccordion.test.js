f1e0cad77db7e14d482225d1c50c3abd3ac02ea5
Sindre Moldeklev <sindre.moldeklev@bekk.no>
2019-03-05 19:01:21 +0100

    Fjernet noen tester og oppdatert snapshot

--- src/Components/Accordion/AboutAccordion.test.js
+++ src/Components/Accordion/AboutAccordion.test.js
@@ -8,32 +8,4 @@ describe("<AboutAccordion />", () => {
     const elem = renderer.create(<AboutAccordion />);
     expect(elem).toMatchSnapshot();
   });
-
-  it("Should handle the activeItem properly", () => {
-    const elem = shallow(<AboutAccordion />);
-    expect(elem.instance().state.activeIndex).toEqual(0);
-    elem.instance().handleClick(null, {index: 1});
-    expect(elem.instance().state.activeIndex).toEqual(1);
-  });
-
-  it("Should mark the correct element as active", () => {
-    const elem = mount(<AboutAccordion />);
-    elem.instance().handleClick(null, {index: 1});
-    expect(elem.find(".active").length).toEqual(2);
-  });
-
-  it("Should correctly calculate the new index", () => {
-    const elem = shallow(<AboutAccordion />);
-    const state = {activeIndex: 2};
-    elem.instance().state = state;
-    let newIndex = elem.instance().calculateNewIndex(2, state.activeIndex);
-    expect(newIndex).toEqual(-1);
-
-    state.activeIndex = 3;
-    newIndex = elem.instance().calculateNewIndex(3, state.activeIndex);
-    expect(newIndex).toEqual(-1);
-
-    newIndex = elem.instance().calculateNewIndex(2, state.activeIndex);
-    expect(newIndex).toEqual(2);
-  });
 });
