c19ce98e10d0b568e2c6596d6321c9bb4fd37848
Sindre Moldeklev <sindre.moldeklev@bekk.no>
2019-03-05 18:58:23 +0100

    Oppdatert til React 16.8 og begynt å introdusere hooks

--- src/Components/Accordion/AboutAccordion.js
+++ src/Components/Accordion/AboutAccordion.js
@@ -1,88 +1,83 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
 import {Accordion, Icon} from "semantic-ui-react";
 
-export default class AccordionExampleStandard extends Component {
-  state = {activeIndex: 0};
+const AboutAccordion = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
 
-  handleClick = (e, titleProps) => {
+  const handleClick = (e, titleProps) => {
     const {index} = titleProps;
-    const {activeIndex} = this.state;
-    const newIndex = this.calculateNewIndex(index, activeIndex);
+    const newIndex = calculateNewIndex(index, activeIndex);
 
-    this.setState({activeIndex: newIndex});
+    setActiveIndex(newIndex);
   };
 
-  calculateNewIndex = (index, activeIndex) => (activeIndex === index ? -1 : index);
+  const calculateNewIndex = (index, activeIndex) =>
+    activeIndex === index ? -1 : index;
 
-  render() {
-    const {activeIndex} = this.state;
+  return (
+    <Accordion>
+      <Accordion.Title
+        active={activeIndex === 0}
+        index={0}
+        onClick={handleClick}
+      >
+        <Icon name="dropdown" />
+        Motivasjon
+      </Accordion.Title>
+      <Accordion.Content active={activeIndex === 0}>
+        <p>
+          Motivasjonen til denne applikasjonen var å bli kvitt repeterende og
+          kjedelig arbeid i forbindelse med forberedelser til FotballXtra.
+        </p>
+        <p>
+          Ideen bak har hele tiden vært og få generert lefsen til FotballXtra
+          med færrest mulige klikk. Fra første versjon av faresone-generatoren
+          så dagens lys som Java-applikasjon i 2014 er dagens versjon oppdatert
+          til å være web-basert. Det betyr at ny funksjonalitet kan komme ut
+          fortere og alt man trenger er en nettleser for tilgang.
+        </p>
+      </Accordion.Content>
 
-    return (
-      <Accordion>
-        <Accordion.Title
-          active={activeIndex === 0}
-          index={0}
-          onClick={this.handleClick}
-        >
-          <Icon name="dropdown" />
-          Motivasjon
-        </Accordion.Title>
-        <Accordion.Content active={activeIndex === 0}>
-          <p>
-            Motivasjonen til denne applikasjonen var å bli kvitt repeterende og
-            kjedelig arbeid i forbindelse med forberedelser til FotballXtra.
-          </p>
-          <p>
-            Ideen bak har hele tiden vært og få generert lefsen til FotballXtra
-            med færrest mulige klikk. Fra første versjon av faresone-generatoren
-            så dagens lys som Java-applikasjon i 2014 er dagens versjon
-            oppdatert til å være web-basert. Det betyr at ny funksjonalitet kan
-            komme ut fortere og alt man trenger er en nettleser for tilgang.
-          </p>
-        </Accordion.Content>
+      <Accordion.Title
+        active={activeIndex === 1}
+        index={1}
+        onClick={handleClick}
+      >
+        <Icon name="dropdown" />
+        Kan det legges til flere ligaer?
+      </Accordion.Title>
+      <Accordion.Content active={activeIndex === 1}>
+        <p>
+          Ja, det kan legges til flere ligaer. Alt man trenger er liga-id og
+          sesong-id fra <a href="http://www.altomfotball.no">altomfotball.no</a>
+          . Foreløpig må det gjøres via Sindre Moldeklev, men i fremtiden kan
+          det være det kan bli gjort av deg som bruker.
+        </p>
+      </Accordion.Content>
 
-        <Accordion.Title
-          active={activeIndex === 1}
-          index={1}
-          onClick={this.handleClick}
-        >
-          <Icon name="dropdown" />
-          Kan det legges til flere ligaer?
-        </Accordion.Title>
-        <Accordion.Content active={activeIndex === 1}>
-          <p>
-            Ja, det kan legges til flere ligaer. Alt man trenger er liga-id og
-            sesong-id fra
-            {" "}
-            <a href="http://www.altomfotball.no">altomfotball.no</a>
-. Foreløpig
-            må det gjøres via Sindre Moldeklev, men i fremtiden kan det være det
-            kan bli gjort av deg som bruker.
-          </p>
-        </Accordion.Content>
+      <Accordion.Title
+        active={activeIndex === 2}
+        index={2}
+        onClick={handleClick}
+      >
+        <Icon name="dropdown" />
+        Hvordan fungerer det?
+      </Accordion.Title>
+      <Accordion.Content active={activeIndex === 2}>
+        <p>
+          Dataene hentes direkte fra samme kilde som Homla og andre systemer i
+          TV 2. Vi kan dermed være sikre på at det vi bruker på luft også er det
+          vi ser på sidene her.
+        </p>
 
-        <Accordion.Title
-          active={activeIndex === 2}
-          index={2}
-          onClick={this.handleClick}
-        >
-          <Icon name="dropdown" />
-          Hvordan fungerer det?
-        </Accordion.Title>
-        <Accordion.Content active={activeIndex === 2}>
-          <p>
-            Dataene hentes direkte fra samme kilde som Homla og andre systemer i
-            TV 2. Vi kan dermed være sikre på at det vi bruker på luft også er
-            det vi ser på sidene her.
-          </p>
+        <p>
+          For Faresone-komponenten for bruk på sending så hentes de spillerne
+          som er i faresonen ned og lagret i nettleseren. Dermed får man besjed
+          dersom
+        </p>
+      </Accordion.Content>
+    </Accordion>
+  );
+};
 
-          <p>
-            For Faresone-komponenten for bruk på sending så hentes de spillerne
-            som er i faresonen ned og lagret i nettleseren. Dermed får man
-            besjed dersom
-          </p>
-        </Accordion.Content>
-      </Accordion>
-    );
-  }
-}
+export default AboutAccordion;
